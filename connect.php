60b6a73a877185cf7128dc42beeaa01a4244509c
Ppo-ppo <pauline-marie.louvrier@outlook.fr>
2022-08-11 15:45:36 +0200

    changement fichier

--- connect.php
+++ connect.php
@@ -1,7 +1,8 @@
-<?php
+<?php 
+session_start();
 function connect(){
     try {
-        $db = new PDO('mysql:host=localhost;dbname=conciergerie', 'root', 'root');
+        $db = new PDO('mysql:host=localhost;dbname=conciergerie','root' ,'root');
         return $db;
         }
     catch (PDOException $e) {
@@ -10,5 +11,23 @@ function connect(){
     }
 }
 
+function login(){
+    $findUser = connect()->prepare('SELECT * FROM `login` WHERE `name` = :login_user');
+    $findUser->bindParam(':login_user', $_POST['username'], PDO::PARAM_STR);
+    $findUser->execute();
+    $user = $findUser->fetch();
+
+    if ($user && password_verify($_POST['password'], $user['password'])) {
+        $_SESSION['nom_user'] = $user['name'];
+        header('Location: ./index.php');  
+        
+    } else {
+        echo 'Invalid username or password';
+    }
+}
+
+if(isset($_POST['action']) && !empty($_POST['username'])  && !empty($_POST['password'])  && $_POST['action']=="login"){
+    login();
+}
 
 ?>
--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login.css">
     <title>Login Conciergerie</title>
 </head>
 <body>
@@ -12,18 +12,17 @@
     <form action="connect.php" class="login" method="post">
         <h1>Login</h1>
         <div class="username">
-            <label for="username">Username:</label>
+            <label for="username">Nom d'utilisateur:</label>
             <input type="text" name="username" id="username">
         </div>
         <div class="password">
-            <label for="password">Password:</label>
+            <label for="password">Mot de passe:</label>
             <input type="password" name="password" id="password">
-        </div>  
+        </div>
         <section>
             <button class="btn" type="submit" name="action" value="login">Login</button>
         </section>
     </form>
-    
-</main>  
+</main>
 </body>
 </html>
